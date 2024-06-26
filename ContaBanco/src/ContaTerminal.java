97aec3e6a7a5950257a73212738652f2af8037f8
Felipe Souza <lipe.jose.souza@gmail.com>
2024-06-26 13:21:54 -0300

    projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        String agencia, nomeCli;
+        double saldo;
+
+        System.out.println("Digite seu nome");
+        nomeCli = sc.nextLine();
+
+        System.out.println("Digite sua agencia.");
+        agencia = sc.nextLine();
+
+        System.out.println("Digite seu numero.");
+        numero = sc.nextInt();
+
+        System.out.println("Digite seu Saldo.");
+        saldo = sc.nextDouble();
+
+
+        System.out.println("Olá " + nomeCli + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia +
+         ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque");
     }
 }
